60d3366be8fbc2981b845f20322b2f76d051bd89
John Wu <me@johnwu.cc>
2017-08-16 23:20:00 +0800

    Add sampleDSL.groovy

---
+++ vars/sampleDSL.groovy
@@ -0,0 +1,12 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    node {
+        echo config.HW
+        echo config.P1
+        echo config.Custom
+    }
+}
